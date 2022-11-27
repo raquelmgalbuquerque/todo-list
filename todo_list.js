19536fc8e5da28ea93df9a652850559546e4fc70
raquelmgalbuquerque <81765141+raquelmgalbuquerque@users.noreply.github.com>
2022-11-27 20:18:23 +0000

    first commit

---
+++ todo_list.js
@@ -0,0 +1,350 @@
+users = [
+  {
+    id: 4839,
+    uid: "f6aff89c-e260-41ef-b444-7b39c050f4f8",
+    password: "RUlbTP3Bn6",
+    first_name: "Mike",
+    last_name: "Rippin",
+    username: "mike.rippin",
+    email: "mike.rippin@email.com",
+    avatar:
+      "https://robohash.org/accusantiumrecusandaebeatae.png?size=300x300&set=set1",
+    gender: "Polygender",
+    phone_number: "+968 (895) 672-1692 x3990",
+    social_insurance_number: "280366048",
+    date_of_birth: "1968-04-13",
+    employment: {
+      title: "Legal Manager",
+      key_skill: "Problem solving",
+    },
+    address: {
+      city: "New Mary",
+      street_name: "Thomas Expressway",
+      street_address: "3958 Ryan Pine",
+      zip_code: "37051-7055",
+      state: "Kansas",
+      country: "United States",
+      coordinates: {
+        lat: -44.11016465972516,
+        lng: -85.9323174462736,
+      },
+    },
+    credit_card: {
+      cc_number: "5481-2564-6953-6879",
+    },
+    subscription: {
+      plan: "Bronze",
+      status: "Idle",
+      payment_method: "Apple Pay",
+      term: "Payment in advance",
+    },
+    tasks: [],
+  },
+  {
+    id: 6028,
+    uid: "ccc742a9-7fdd-4b9f-a2ff-cb6dad0a2cf5",
+    password: "MzjJGbOkN1",
+    first_name: "Tyron",
+    last_name: "Willms",
+    username: "tyron.willms",
+    email: "tyron.willms@email.com",
+    avatar: "https://robohash.org/undemolestiaeesse.png?size=300x300&set=set1",
+    gender: "Male",
+    phone_number: "+678 738-346-8025 x554",
+    social_insurance_number: "519529630",
+    date_of_birth: "1995-05-12",
+    employment: {
+      title: "Central Engineer",
+      key_skill: "Problem solving",
+    },
+    address: {
+      city: "North Lucianostad",
+      street_name: "Murphy Mount",
+      street_address: "733 Morar Parkway",
+      zip_code: "04215",
+      state: "Wyoming",
+      country: "United States",
+      coordinates: {
+        lat: 53.76425399306825,
+        lng: 133.64452330346802,
+      },
+    },
+    credit_card: {
+      cc_number: "4373536188920",
+    },
+    subscription: {
+      plan: "Standard",
+      status: "Pending",
+      payment_method: "Cheque",
+      term: "Full subscription",
+    },
+    tasks: [],
+  },
+  {
+    id: 3663,
+    uid: "8f867b47-ac85-4856-a92b-be72d43d3121",
+    password: "VIt6XLvRuP",
+    first_name: "Delma",
+    last_name: "Effertz",
+    username: "delma.effertz",
+    email: "delma.effertz@email.com",
+    avatar:
+      "https://robohash.org/molestiaeoptiorerum.png?size=300x300&set=set1",
+    gender: "Bigender",
+    phone_number: "+672 (633) 158-4000 x62688",
+    social_insurance_number: "707564985",
+    date_of_birth: "1965-03-29",
+    employment: {
+      title: "Human Mining Technician",
+      key_skill: "Leadership",
+    },
+    address: {
+      city: "South Ralph",
+      street_name: "Lyman Lodge",
+      street_address: "7423 Grimes Plaza",
+      zip_code: "67598",
+      state: "Utah",
+      country: "United States",
+      coordinates: {
+        lat: -1.4433291423956547,
+        lng: -11.028647089289706,
+      },
+    },
+    credit_card: {
+      cc_number: "5389-6640-5738-2429",
+    },
+    subscription: {
+      plan: "Standard",
+      status: "Blocked",
+      payment_method: "Paypal",
+      term: "Annual",
+    },
+    tasks: [],
+  },
+  {
+    id: 9547,
+    uid: "16e11c75-0900-486e-beaa-3dba373a3261",
+    password: "gIFTR7D2Yj",
+    first_name: "Fe",
+    last_name: "Marquardt",
+    username: "fe.marquardt",
+    email: "fe.marquardt@email.com",
+    avatar: "https://robohash.org/aperiamvelquae.png?size=300x300&set=set1",
+    gender: "Non-binary",
+    phone_number: "+298 725-651-9517 x75995",
+    social_insurance_number: "660464397",
+    date_of_birth: "1974-10-19",
+    employment: {
+      title: "Design Agent",
+      key_skill: "Work under pressure",
+    },
+    address: {
+      city: "Goldnerborough",
+      street_name: "Brian Spring",
+      street_address: "583 Larkin Branch",
+      zip_code: "70801-3900",
+      state: "Massachusetts",
+      country: "United States",
+      coordinates: {
+        lat: 83.32296392552979,
+        lng: -169.92995348309458,
+      },
+    },
+    credit_card: {
+      cc_number: "5150-2778-8809-6022",
+    },
+    subscription: {
+      plan: "Basic",
+      status: "Active",
+      payment_method: "Credit card",
+      term: "Full subscription",
+    },
+    tasks: [],
+  },
+  {
+    id: 1098,
+    uid: "8310065d-7465-4fcc-899b-028684d8cee7",
+    password: "iEVMD5Gbo4",
+    first_name: "Ellsworth",
+    last_name: "Lueilwitz",
+    username: "ellsworth.lueilwitz",
+    email: "ellsworth.lueilwitz@email.com",
+    avatar: "https://robohash.org/utetdoloremque.png?size=300x300&set=set1",
+    gender: "Genderfluid",
+    phone_number: "+967 1-578-535-1458",
+    social_insurance_number: "348344987",
+    date_of_birth: "1960-08-25",
+    employment: {
+      title: "Legal Developer",
+      key_skill: "Self-motivated",
+    },
+    address: {
+      city: "West Sibylburgh",
+      street_name: "Dallas Fields",
+      street_address: "1228 Luettgen Track",
+      zip_code: "17721",
+      state: "South Dakota",
+      country: "United States",
+      coordinates: {
+        lat: -38.11371220149862,
+        lng: 20.08226566387748,
+      },
+    },
+    credit_card: {
+      cc_number: "4924-5382-4659-3374",
+    },
+    subscription: {
+      plan: "Standard",
+      status: "Pending",
+      payment_method: "Money transfer",
+      term: "Payment in advance",
+    },
+    tasks: [],
+  },
+];
+
+console.log(users);
+
+let userSelect = document.querySelector(".user-select");
+
+users.forEach((user) => {
+  let userSelectOption = document.createElement("option");
+  let fullName = `${user.first_name} ${user.last_name}`;
+  userSelectOption.value = user.id;
+  userSelectOption.innerHTML = fullName;
+  userSelect.appendChild(userSelectOption);
+});
+
+function calculateAge(birthday) {
+  birthday = new Date(birthday);
+  let ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs);
+  let age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+  return age;
+}
+
+function displayUser() {
+  let userId = document.querySelector(".user-select").value;
+  let foundUser = users.find((user) => user.id == userId);
+  let userInfoElements = document.querySelectorAll(".user-info");
+  for (let i = 0; i < userInfoElements.length; i++) {
+    userInfoElements[i].removeAttribute("hidden");
+  }
+
+  document.querySelector(".user-image").src = foundUser.avatar;
+  document.querySelector(".user-age span").innerHTML = calculateAge(
+    foundUser.date_of_birth
+  );
+  document.querySelector(".user-birthday span").innerHTML =
+    foundUser.date_of_birth;
+  document.querySelector(".user-city span").innerHTML = foundUser.address.city;
+  document.querySelector(".user-country span").innerHTML =
+    foundUser.address.country;
+  document.querySelector(".user-profession span").innerHTML =
+    foundUser.employment.title;
+
+  document.querySelector(
+    ".task-form-container"
+  ).innerHTML = `<div class="title-add-new-task">Add a new task</div>
+  <form class="task-form" onsubmit="addTask(event)">
+    <input type="hidden" id="task-form-userId" name="task-form-userId" value="${userId}">
+    <label for="title">Task title:</label>
+    <input type="text" name="title" id="task-form-title" required />
+    <br />
+
+    <label for="deadline">Deadline:</label>
+    <input type="date" name="deadline" id="task-form-deadline" required />
+    <br />
+
+    <label for="description">Description:</label>
+    <input
+      type="text"
+      name="description"
+      id="task-form-description"
+      required
+    />
+    <br />
+
+    <button class="button-add-task" type="submit">Add Task</button>
+  </form>`;
+
+  displayTasks(foundUser.tasks, userId);
+}
+
+function addTask(event) {
+  event.preventDefault();
+
+  let taskUserId = document.querySelector("#task-form-userId").value;
+  let taskTitle = document.querySelector("#task-form-title").value;
+  let taskDeadline = document.querySelector("#task-form-deadline").value;
+  let taskDescription = document.querySelector("#task-form-description").value;
+
+  let foundUser = users.find((user) => user.id == taskUserId);
+
+  let taskCreationDate = new Date().toISOString().slice(0, 10);
+
+  let task = {
+    id: Date.now(),
+    createdAt: taskCreationDate,
+    title: taskTitle,
+    deadline: taskDeadline,
+    description: taskDescription,
+  };
+
+  foundUser.tasks.push(task);
+
+  displayTasks(foundUser.tasks, taskUserId);
+}
+
+function removeTask(taskId, userId) {
+  let foundUser = users.find((user) => user.id == userId);
+  let taskIndex = foundUser.tasks.findIndex((task) => task.id == taskId);
+  foundUser.tasks.splice(taskIndex, 1);
+
+  console.log(foundUser.tasks);
+
+  displayTasks(foundUser.tasks, userId);
+}
+
+function deadlineValidator(task) {
+  let deadline = new Date(task.deadline);
+  let today = new Date(new Date().toISOString().slice(0, 10));
+  const diffTime = Math.abs(today - deadline);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+  let delayColor =
+    diffDays <= 1 ? "red" : diffDays > 1 && diffDays <= 3 ? "orange" : null;
+
+  return delayColor;
+}
+
+function displayTasks(tasks, userId) {
+  let taskTableContainer = document.querySelector(".task-list-container");
+  taskTableContainer.innerHTML = "";
+
+  let tableElement = document.createElement("table");
+  tableElement.innerHTML = `<tr>
+    <th>Title</th>
+    <th>Created at</th>
+    <th>Deadline</th>
+    <th>Description</th>
+    <th>Options</th>
+  </tr>`;
+  taskTableContainer.appendChild(tableElement);
+
+  // The ID corresponds to the timestamp that the task was created. So if you sort by ID, you are sorting by creation date!
+  tasks.sort((a, b) => b.id - a.id);
+
+  tasks.map((task) => {
+    let delayColor = deadlineValidator(task);
+    let tableRow = document.createElement("tr");
+    tableRow.innerHTML = `<td>${task.title}</td>
+    <td>${task.createdAt}</td>
+    <td style="background-color:${delayColor};">${task.deadline}</td>
+    <td>${task.description}</td>
+    <td class="table-options"><button type="submit" onclick="removeTask(${task.id}, ${userId})">Remove</button></td>`;
+
+    tableElement.appendChild(tableRow);
+  });
+}
